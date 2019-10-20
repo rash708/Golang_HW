3fb1a3b06bc7e2d830fedef40d75a80124973ca7
rash708 <32931893+rash708@users.noreply.github.com>
2019-10-20 23:37:56 +0300

    Попытка распапалелить процессы

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -2,41 +2,83 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
+	"sync"
+	"time"
 )
 
+var result string = ""
+
+func worker(j job, in, out chan interface{}, wg *sync.WaitGroup) {
+	defer wg.Done() // уменьшаем счетчик на 1
+	defer close(out)
+	j(in, out)
+
+}
+
 //ExecutePipeline SingleHash -> Multihash -> CombineResults
-func ExecutePipeline(data string) {
-	CombineResults(MultiHash(SingleHash(data)))
+func ExecutePipeline(f ...job) {
+	wg := &sync.WaitGroup{} // инициализируем группу
+	wg.Add(1)               // добавляем воркер
+	in := make(chan interface{}, 50)
+	out := make(chan interface{}, 50)
+	for _, val := range f {
+		go worker(val, in, out, wg)
+	}
+	time.Sleep(time.Millisecond)
+	wg.Wait() // ожидаем, пока waiter.Done() не приведёт счетчик к 0
 }
 
 //SingleHash crc32(data)+"~"+crc32(md5(data))
-func SingleHash(data string) string {
-	return DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+func SingleHash(in, out chan interface{}) {
+	in <- out
+	data := <-in
+	fmt.Println(DataSignerCrc32(data.(string)) + "~" + DataSignerCrc32(DataSignerMd5(data.(string))))
+	out <- DataSignerCrc32(data.(string)) + "~" + DataSignerCrc32(DataSignerMd5(data.(string)))
 }
 
-//MultiHash crc32(th+data)), где data - htpkmnfn
-func MultiHash(data string) string {
-	var out string
+//MultiHash crc32(th+data)), где data - резульат SingleHash
+func MultiHash(in, out chan interface{}) {
 	var temp string
+	var cur string
+
+	in <- out
+	data := <-in
+
 	for th := 0; th < 6; th++ {
-		temp = DataSignerCrc32(strconv.Itoa(th) + data)
-		fmt.Printf("%v "+temp+"\n", th)
-		out += temp
+		cur = DataSignerCrc32(strconv.Itoa(th) + data.(string))
+		fmt.Printf("%v "+cur+"\n", th)
+		temp += cur
 	}
-	return out
+	fmt.Println(temp)
+	out <- temp
 }
 
 //CombineResults res1_res2_.._resN
-func CombineResults(data string) string {
-	out := DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
-	return out
+func CombineResults(in, out chan interface{}) {
+	in <- out
+	data := <-in
+
+	result += data.(string)
 }
 
 func main() {
-	fmt.Println(SingleHash(os.Args[1]))
-	fmt.Println(MultiHash(SingleHash(os.Args[1])))
+	Hash := []job{
+		job(func(in, out chan interface{}) {
+			in <- uint32(0)
+			in <- uint32(1)
+		}),
+		job(SingleHash),
+		job(MultiHash),
+		job(CombineResults),
+	}
+
+	ExecutePipeline(Hash...)
+
+	fmt.Println(result)
+
+	//fmt.Println(SingleHash(os.Args[1]))
+	//fmt.Println(MultiHash(SingleHash(os.Args[1])))
 }
 
 // сюда писать код
