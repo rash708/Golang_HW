ad602bed9a85a736c67641c8b2fe0a0effaa9640
rash708 <32931893+rash708@users.noreply.github.com>
2019-10-22 13:23:43 +0300

    MVP solution
    Реализация с горутинами

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -2,39 +2,45 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"sync"
-	"time"
 )
-
-var result string = ""
 
 func worker(j job, in, out chan interface{}, wg *sync.WaitGroup) {
 	defer wg.Done() // уменьшаем счетчик на 1
 	defer close(out)
 	j(in, out)
-
 }
 
 //ExecutePipeline SingleHash -> Multihash -> CombineResults
-func ExecutePipeline(f ...job) {
+func ExecutePipeline(f ...job) error {
 	wg := &sync.WaitGroup{} // инициализируем группу
-	wg.Add(1)               // добавляем воркер
-	in := make(chan interface{}, 50)
-	out := make(chan interface{}, 50)
+	in := make(chan interface{}, 100)
+	out := make(chan interface{}, 100)
 	for _, val := range f {
+		wg.Add(1) // добавляем воркер
 		go worker(val, in, out, wg)
+
+		//Реализация конвеера ->in:out->in:out->in:out
+		in = out
+		outNew := make(chan interface{}, 100)
+		out = outNew
 	}
-	time.Sleep(time.Millisecond)
 	wg.Wait() // ожидаем, пока waiter.Done() не приведёт счетчик к 0
+
+	return nil
 }
 
 //SingleHash crc32(data)+"~"+crc32(md5(data))
 func SingleHash(in, out chan interface{}) {
-	in <- out
-	data := <-in
-	fmt.Println(DataSignerCrc32(data.(string)) + "~" + DataSignerCrc32(DataSignerMd5(data.(string))))
-	out <- DataSignerCrc32(data.(string)) + "~" + DataSignerCrc32(DataSignerMd5(data.(string)))
+	for dataRaw := range in {
+		dataTmp, _ := dataRaw.(int)
+		data := strconv.Itoa(dataTmp)
+		res := DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+		fmt.Println(res)
+		out <- res
+	}
 }
 
 //MultiHash crc32(th+data)), где data - резульат SingleHash
@@ -42,31 +48,54 @@ func MultiHash(in, out chan interface{}) {
 	var temp string
 	var cur string
 
-	in <- out
-	data := <-in
+	for dataRaw := range in {
+		data, _ := dataRaw.(string)
 
-	for th := 0; th < 6; th++ {
-		cur = DataSignerCrc32(strconv.Itoa(th) + data.(string))
-		fmt.Printf("%v "+cur+"\n", th)
-		temp += cur
+		temp = ""
+		cur = ""
+
+		for th := 0; th < 6; th++ {
+			cur = DataSignerCrc32(strconv.Itoa(th) + data)
+			fmt.Printf("%v "+cur+"\n", th)
+			temp += cur
+		}
+		fmt.Println(temp)
+		out <- temp
 	}
-	fmt.Println(temp)
-	out <- temp
 }
 
 //CombineResults res1_res2_.._resN
 func CombineResults(in, out chan interface{}) {
-	in <- out
-	data := <-in
+	var arr []string
+	var str string
 
-	result += data.(string)
+	for dataRaw := range in {
+		data, _ := dataRaw.(string)
+		arr = append(arr, data)
+	}
+
+	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
+	for pos, temp := range arr {
+		if pos == 0 {
+			str += temp
+		} else {
+			str += "_" + temp
+		}
+	}
+
+	//fmt.Printf("%v \n", arr)
+	fmt.Printf("%v", str)
+	out <- str
 }
 
+//Тестирование
 func main() {
+	inputData := []int{0, 1}
 	Hash := []job{
 		job(func(in, out chan interface{}) {
-			in <- uint32(0)
-			in <- uint32(1)
+			for _, fibNum := range inputData {
+				out <- fibNum
+			}
 		}),
 		job(SingleHash),
 		job(MultiHash),
@@ -74,11 +103,4 @@ func main() {
 	}
 
 	ExecutePipeline(Hash...)
-
-	fmt.Println(result)
-
-	//fmt.Println(SingleHash(os.Args[1]))
-	//fmt.Println(MultiHash(SingleHash(os.Args[1])))
 }
-
-// сюда писать код
