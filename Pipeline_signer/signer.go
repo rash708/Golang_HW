f3b789ffa7eeff097708ff9873a322195307a874
rash708 <32931893+rash708@users.noreply.github.com>
2019-10-22 18:39:38 +0300

    Optimize
    Время выполнение с 44сек до 8 сек

--- Pipeline_signer/signer.go
+++ Pipeline_signer/signer.go
@@ -13,6 +13,11 @@ func worker(j job, in, out chan interface{}, wg *sync.WaitGroup) {
 	j(in, out)
 }
 
+// func single(j func(string) string, in, out chan string) {
+// 	data := <-in
+// 	out <- j(data)
+// }
+
 //ExecutePipeline SingleHash -> Multihash -> CombineResults
 func ExecutePipeline(f ...job) error {
 	wg := &sync.WaitGroup{} // инициализируем группу
@@ -34,33 +39,91 @@ func ExecutePipeline(f ...job) error {
 
 //SingleHash crc32(data)+"~"+crc32(md5(data))
 func SingleHash(in, out chan interface{}) {
+
+	//type hash func(func(data string) string, j job)
+
+	// Conv := []hash{
+	// 	job(SingleHash),
+	// 	job(MultiHash),
+	// 	job(CombineResults),
+	// }
+
+	//mu := &sync.Mutex{}
 	for dataRaw := range in {
+		var resTemp = make(map[string]string)
 		dataTmp, _ := dataRaw.(int)
 		data := strconv.Itoa(dataTmp)
-		res := DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+
+		// in := make(chan string, 10)
+		// out := make(chan string, 10)
+		// in <- data
+
+		// go single(DataSignerMd5, in, out)
+		// go single(DataSignerCrc32, out, in)
+
+		mu := &sync.Mutex{}
+		wg := &sync.WaitGroup{} // инициализируем группу
+
+		wg.Add(2) // добавляем воркер
+
+		go func(res map[string]string, mu *sync.Mutex, wg *sync.WaitGroup) {
+			defer wg.Done()
+			Md5 := DataSignerCrc32(DataSignerMd5(data))
+			mu.Lock()
+			res["Md5"] = Md5
+			mu.Unlock()
+		}(resTemp, mu, wg)
+
+		go func(res map[string]string, mu *sync.Mutex, wg *sync.WaitGroup) {
+			defer wg.Done()
+			crc32 := DataSignerCrc32(data)
+			//crc32Md5 := DataSignerCrc32(res["Md5"])
+			mu.Lock()
+			res["crc32"] = crc32
+			//res["Md5"] = crc32Md5
+			mu.Unlock()
+		}(resTemp, mu, wg)
+
+		wg.Wait()
+
+		res := resTemp["crc32"] + "~" + resTemp["Md5"]
 		fmt.Println(res)
 		out <- res
+
 	}
 }
 
 //MultiHash crc32(th+data)), где data - резульат SingleHash
 func MultiHash(in, out chan interface{}) {
-	var temp string
-	var cur string
-
 	for dataRaw := range in {
 		data, _ := dataRaw.(string)
 
-		temp = ""
-		cur = ""
+		var temp = make(map[int]string)
+		cur := ""
+
+		mu := &sync.Mutex{}
+		wg := &sync.WaitGroup{} // инициализируем группу
+
+		//wg.Add(1) // добавляем воркер
 
 		for th := 0; th < 6; th++ {
-			cur = DataSignerCrc32(strconv.Itoa(th) + data)
-			fmt.Printf("%v "+cur+"\n", th)
-			temp += cur
+			wg.Add(1) // добавляем воркер
+			go func(th int, data, cur string, temp map[int]string, wg *sync.WaitGroup, mu *sync.Mutex) {
+				cur = DataSignerCrc32(strconv.Itoa(th) + data)
+				fmt.Printf("%v "+cur+"\n", th)
+				mu.Lock()
+				temp[th] = cur
+				mu.Unlock()
+				wg.Done() // уменьшаем счетчик на 1
+			}(th, data, cur, temp, wg, mu)
+
 		}
-		fmt.Println(temp)
-		out <- temp
+
+		wg.Wait()
+
+		//fmt.Println(temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5])
+		out <- (temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5])
+		//out <- temp
 	}
 }
 
